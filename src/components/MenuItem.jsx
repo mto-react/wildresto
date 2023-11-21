d232587dde1e0e6cd2227c61df395f7f823409a5
mtoan <mto.work@outlook.com>
2023-11-21 15:59:04 +0100

    commit

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function MenuItem(props) {
+function MenuItem({foodItem}) {
   //create a state isFavorite that has the inital value of isFavorite that comes from the props
+  //   console.log(foodItem)
+  const [favor, setFavor] = useState(foodItem.isFavorite)
 
   return (
     <div className="itemContainer">
       <div className="leftContainer">
         <div className="imgContainer">
           {/* the image will receive the url src from the props */}
-          <img src="" alt="" />
+          <img src={foodItem.foodImage} alt="In coming" size="200x200" />
         </div>
         <div className="itemDescription">
           {/* the h3 will receive the item name from the props */}
-          <h3>{}</h3>
+          <h3>{foodItem.description}</h3>
           {/* the p will receive the item description from the props */}
           <p>{}</p>
         </div>
       </div>
       <div className="rightContainer">
         {/* the div will receive the item price from the props */}
-        <div>{} EUR</div>
+        <div>{foodItem.price} EUR</div>
 
         {/* the div with id favorite will have 2 attributes:
                 - onClick, will call the method handleClickFavorite,
                 - classname, that will be conditionally rendered, depending on the value of isFavorite from the component's state
             */}
-        <div id="favorite" />
+        <div id="favorite" 
+            className={favor ? 'isFavorite' : 'notFavorite'}
+            onClick={() => setFavor(!favor)} 
+        />
       </div>
     </div>
   );
--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -5,7 +5,13 @@ function MenuList({ foodItems }) {
   console.log(foodItems);
   return (
     <div>
-      {/*render a MenuItem component to each element of the props array*/}
+      {/*render a MenuItem component to each element of the props array*/
+        foodItems.map((foodItem) => (
+            <div key = {foodItem.itemName}>
+                <MenuItem foodItem = {foodItem} />
+            </div>
+        ))
+      }
     </div>
   );
 }
